7f32530f95e2f9c845a7bfbd5d837d5e63e4339c
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-09-19 16:00:15 -0500

    final udate

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 const listaIdeas = document.querySelector('#lista-ideas');
 const form = document.querySelector('#formulario');
+let ideas = [];
 
 
 // event listeners
@@ -11,10 +12,19 @@ eveentListeners();
 function eveentListeners() {
 
     // when the form is submitted
-
     form.addEventListener('submit', agregarIdea);
-}
 
+    // when the document is loaded
+
+    document.addEventListener('DOMContentLoaded', () => {
+        ideas = JSON.parse(localStorage.getItem('ideas')) || [];
+        createHTML();
+    });
+
+    // delete idea
+
+    listaIdeas.addEventListener('click', deleteIdea);
+}
 
 // functions 
 
@@ -22,21 +32,85 @@ function agregarIdea(e) {
     e.preventDefault();
 
     const idea = document.querySelector('#idea').value;
+    // console.log(idea);
     
     if(idea === '') {
-        showError('Una idea no puede ir vacía');
+        showError('Agrega una idea');
         return;
     }
+
+    // create a object with the idea
+
+    const ideaObj = {
+        id: Date.now(),
+        texto: idea
+    }
+    // add the idea to the array
+    ideas = [...ideas, ideaObj];
+
+    console.log(ideas);
+
+    // rendering the ideas
+    createHTML();
+
+    // restart the form
+
+    form.reset();
 }
 
 function showError (error) {
-    
     const messageError = document.createElement('p');
     messageError.textContent = error;
     messageError.classList.add('error');
+    messageError.classList.add('show');
 
     const contenido = document.querySelector('#contenido');
-
     contenido.appendChild(messageError);
 
+    setTimeout(() => {
+        messageError.remove();
+    }, 3000);
+
+}
+
+function createHTML() {
+
+    clearHTML();
+
+    if(ideas.length > 0) {
+        ideas.forEach(idea => {
+            const btnEliminar = document.createElement('a');
+            btnEliminar.classList.add('borrar-idea');
+            btnEliminar.innerText = 'X';
+
+            // function to delete idea
+            btnEliminar.onclick = () => {
+                deleteIdea(idea.id);
+            }
+
+            const li = document.createElement('li');
+            li.innerText = idea.texto;
+
+            li.appendChild(btnEliminar);
+            listaIdeas.appendChild(li);
+        });
+    }
+    syncStorage();
+}
+
+function clearHTML() {
+    while(listaIdeas.firstChild) {
+        listaIdeas.removeChild(listaIdeas.firstChild);
+    }
+}
+
+function deleteIdea(id) {
+    
+
+    ideas = ideas.filter(idea => idea.id !== id);
+    createHTML();
+}
+
+function syncStorage() {
+    localStorage.setItem('ideas', JSON.stringify(ideas));
 }
